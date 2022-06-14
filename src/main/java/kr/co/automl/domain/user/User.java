dd8c6df22a030fb31e21fda3f3cad9b6afbcfb08
pangnem <sprnd645@gmail.com>
2022-06-14 16:36:45 +0900

    feat: User 도메인에 누락된 이메일 필드 추가 #56

--- src/main/java/kr/co/automl/domain/user/User.java
+++ src/main/java/kr/co/automl/domain/user/User.java
@@ -4,12 +4,14 @@ public class User {
     private final Long id;
     private final String name;
     private final String imageUrl;
+    private final String email;
     private final Role role;
 
-    public User(Long id, String name, String imageUrl, Role role) {
+    public User(Long id, String name, String imageUrl, String email, Role role) {
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
+        this.email = email;
         this.role = role;
     }
 }
