f49adc9eab51b40c36ebb670ae8bce6fe5d21277
pangnem <sprnd645@gmail.com>
2022-06-15 22:27:15 +0900

    feat: 임시 dashbaord page 컴포넌트 및 라우팅 추가
    추후 개발 예정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
+function DashBoardPage() {
+  return <>
+    <div>
+      ToDo: 개발 예정...
+    </div>
+  </>;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
+        <Route path="/dashboard" element={<DashBoardPage/>}/>
       </Routes>
     </BrowserRouter>
   );
