1bf96c095914e554b8d2ed9f0dd6f60bebfb290b
pangnem <sprnd645@gmail.com>
2022-06-16 16:34:57 +0900

    feat: Custom OAuth2UserService
    인증 완료 이후 작업들을 처리

---
+++ src/main/java/kr/co/automl/global/config/CustomOAuth2UserService.java
@@ -0,0 +1,58 @@
+package kr.co.automl.global.config;
+
+import kr.co.automl.domain.user.User;
+import kr.co.automl.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * 인증 완료 이후 작업들을 처리.
+ */
+@RequiredArgsConstructor
+@Component
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final HttpSession httpSession;
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = new DefaultOAuth2UserService()
+                .loadUser(userRequest);
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        User user = saveOrUpdateUser(attributes);
+        httpSession.setAttribute("user", user.toSessionUser());
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleName())),
+                attributes,
+                userRequest.getClientRegistration()
+                        .getProviderDetails()
+                        .getUserInfoEndpoint()
+                        .getUserNameAttributeName()
+        );
+    }
+
+    private User saveOrUpdateUser(Map<String, Object> attributes) {
+        String name = (String) attributes.get("name");
+        String imageUrl = (String) attributes.get("picture");
+        String email = (String) attributes.get("email");
+
+        User user = userRepository.findByEmail(email)
+                .orElse(User.ofDefaultRole(name, imageUrl, email));
+
+        return userRepository.save(user);
+    }
+}
--- src/main/java/kr/co/automl/global/config/SecurityConfig.java
+++ src/main/java/kr/co/automl/global/config/SecurityConfig.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthenticationSuccessHandler loginSuccessRedirector;
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .and()
                 .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService)
+                .and()
                 .successHandler(loginSuccessRedirector);
     }
 }
