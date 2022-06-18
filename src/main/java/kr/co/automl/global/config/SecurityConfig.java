71d615e5d4ca34c43218a885ce48d5c1f660b2a4
pangnem <sprnd645@gmail.com>
2022-06-18 17:50:07 +0900

    refactor: 불필요한 어노테이션 제거
    `@EnableWebSecurity`

--- src/main/java/kr/co/automl/global/config/SecurityConfig.java
+++ src/main/java/kr/co/automl/global/config/SecurityConfig.java
@@ -4,15 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final OAuth2UserService<OAuth2UserRequest, OAuth2User> customOAuth2UserService;
