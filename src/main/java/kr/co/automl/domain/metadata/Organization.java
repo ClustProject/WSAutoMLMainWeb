1a8064a246dcac381802ee5d7ebe3318851ae706
pangnem <sprnd645@gmail.com>
2022-06-22 21:06:19 +0900

    feat: 컨소기관 상수 추가 #117
    구축기관 String 컨소기관 상수로 변경
    
    ---
    Close #117

--- src/main/java/kr/co/automl/domain/metadata/Organization.java
+++ src/main/java/kr/co/automl/domain/metadata/Organization.java
@@ -7,7 +7,7 @@ public record Organization(
         /* 제공기관 */
         String publisher,
         /* 컨소기관 */
-        String creator,
+        Creator creator,
         /* 연락처 */
         ContactPoint contactPoint
 ) {
