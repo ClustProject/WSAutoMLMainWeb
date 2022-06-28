6580c0c388ba5f70fc6e37df77f8cbfc0c3b5bab
pangnem <sprnd645@gmail.com>
2022-06-28 16:17:52 +0900

    fix: 누락된 DataSet 파일 추가

---
+++ src/main/java/kr/co/automl/domain/metadata/dataset/DataSet.java
@@ -0,0 +1,49 @@
+package kr.co.automl.domain.metadata.dataset;
+
+import kr.co.automl.domain.metadata.BaseTimeEntity;
+import kr.co.automl.domain.metadata.LicenseInfo;
+import kr.co.automl.domain.metadata.Organization;
+import kr.co.automl.domain.metadata.Type;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+public class DataSet extends BaseTimeEntity {
+
+    private long id;
+
+    private String title;
+    private Organization organization;
+    private Type type;
+    private String keyword;
+    private LicenseInfo licenseInfo;
+    private String description;
+
+    public static DataSet create(
+            String title,
+            String publisher,
+            String creatorName,
+            String contactPointName,
+            String typeName,
+            String keyword,
+            String licenseName,
+            String rightsName,
+            String description
+    ) {
+        Organization organization = Organization.of(publisher, creatorName, contactPointName);
+
+        Type type = Type.ofName(typeName);
+
+        LicenseInfo licenseInfo = LicenseInfo.of(licenseName, rightsName);
+
+        return new DataSet(title, organization, type, keyword, licenseInfo, description);
+    }
+
+    DataSet(String title, Organization organization, Type type, String keyword, LicenseInfo licenseInfo, String description) {
+        this.title = title;
+        this.organization = organization;
+        this.type = type;
+        this.keyword = keyword;
+        this.licenseInfo = licenseInfo;
+        this.description = description;
+    }
+}
---
+++ src/main/java/kr/co/automl/domain/metadata/exceptions/CannotFindMatchTypeException.java
@@ -0,0 +1,8 @@
+package kr.co.automl.domain.metadata.exceptions;
+
+public class CannotFindMatchTypeException extends RuntimeException {
+
+    public CannotFindMatchTypeException(String name) {
+        super(String.format("일치하는 매체 유형을 찾을 수 없습니다: %s", name));
+    }
+}
