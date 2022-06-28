4d428a3ed656fb29fa62b0f123e732cf2447a625
pangnem <sprnd645@gmail.com>
2022-06-28 16:25:16 +0900

    refactor: 데이터셋 관련 예외 dataset 패키지로 이동

--- src/main/java/kr/co/automl/domain/metadata/dataset/exceptions/CannotFindMatchCreatorException.java
+++ src/main/java/kr/co/automl/domain/metadata/dataset/exceptions/CannotFindMatchCreatorException.java
@@ -1,4 +1,4 @@
-package kr.co.automl.domain.metadata.exceptions;
+package kr.co.automl.domain.metadata.dataset.exceptions;
 
 public class CannotFindMatchCreatorException extends RuntimeException {
 
