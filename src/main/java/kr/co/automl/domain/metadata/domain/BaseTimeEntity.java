b2ffee60e89b42d8fcfa22cdc7cba96100b8693e
pangnem <sprnd645@gmail.com>
2022-07-01 15:56:10 +0900

    fix: 날짜가 null로 들어가는 문제 수정
    See Also:
    - https://craftdeveloper.tistory.com/16

--- src/main/java/kr/co/automl/domain/metadata/domain/BaseTimeEntity.java
+++ src/main/java/kr/co/automl/domain/metadata/domain/BaseTimeEntity.java
@@ -2,7 +2,9 @@ package kr.co.automl.domain.metadata.domain;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDate;
 
@@ -10,6 +12,7 @@ import java.time.LocalDate;
  * 시간 객체
  */
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
     @CreatedDate
