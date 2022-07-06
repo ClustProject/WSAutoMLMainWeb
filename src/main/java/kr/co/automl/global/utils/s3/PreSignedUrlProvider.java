89bfe2805477976f49ad75a5a5644899a7650525
pangnem <sprnd645@gmail.com>
2022-07-06 17:08:08 +0900

    feat: 파일 이름이 빈값인 케이스 추가

--- src/main/java/kr/co/automl/global/utils/s3/PreSignedUrlProvider.java
+++ src/main/java/kr/co/automl/global/utils/s3/PreSignedUrlProvider.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import java.net.URL;
 
@@ -24,6 +25,8 @@ public class PreSignedUrlProvider {
      * @return 생성한 PreSignedUrl
      */
     public String getWithFilename(String filename) {
+        validateEmpty(filename);
+
         GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(
                 bucketName,
                 getKey(filename),
@@ -33,6 +36,18 @@ public class PreSignedUrlProvider {
         URL url = amazonS3Client.generatePresignedUrl(request);
 
         return url.toString();
+    }
+
+    /**
+     * 비어있는지 검증합니다.
+     * @param filename 검증할 파일 이름
+     *
+     * @throws IllegalArgumentException 파일 이름 검증에 실패할경우
+     */
+    private void validateEmpty(String filename) {
+        if (ObjectUtils.isEmpty(filename)) {
+            throw new IllegalArgumentException("파일이름은 비어있을 수 없습니다.");
+        }
     }
 
     String getKey(String filename) {
