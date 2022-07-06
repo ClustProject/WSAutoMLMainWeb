27fea47d73e5dc64c09f84b108df2f78f7b0e77f
pangnem <sprnd645@gmail.com>
2022-07-06 19:32:56 +0900

    fix: amazonS3Client에 설정값의 access, secret key를 사용하도록 변경

--- src/main/java/kr/co/automl/global/config/s3/S3Config.java
+++ src/main/java/kr/co/automl/global/config/s3/S3Config.java
@@ -1,6 +1,9 @@
 package kr.co.automl.global.config.s3;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +16,12 @@ public class S3Config {
     @Value("${cloud.aws.s3.bucketName}")
     private String bucketName;
 
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -23,9 +32,12 @@ public class S3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(region)
+                .withCredentials(credentialsProvider)
                 .build();
     }
 }
