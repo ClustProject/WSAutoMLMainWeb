0d18bf506cc6bfa318b4e4f66eebcfa21e5c6fc9
pangnem <sprnd645@gmail.com>
2022-07-08 17:18:08 +0900

    feat: 메타데이터 입력시 모든 요소들을 빈값 허용하지 않도록 설정
    dto에 notBlank 어노테이션 추가

--- src/main/java/kr/co/automl/domain/metadata/domain/dataset/dto/CreateDataSetAttributes.java
+++ src/main/java/kr/co/automl/domain/metadata/domain/dataset/dto/CreateDataSetAttributes.java
@@ -7,8 +7,10 @@ import javax.validation.constraints.NotBlank;
 import static kr.co.automl.domain.metadata.dto.CreateMetaDataAttributes.NOT_BLANK_MESSAGE_SUFFIX;
 
 public record CreateDataSetAttributes(
+        @NotBlank(message = "데이터셋 제목" + NOT_BLANK_MESSAGE_SUFFIX)
         String title,
 
+        @NotBlank(message = "제공 기관" + NOT_BLANK_MESSAGE_SUFFIX)
         String publisher,
 
         @NotBlank(message = "생성 기관" + NOT_BLANK_MESSAGE_SUFFIX)
@@ -20,6 +22,7 @@ public record CreateDataSetAttributes(
         @NotBlank(message = "유형" + NOT_BLANK_MESSAGE_SUFFIX)
         String type,
 
+        @NotBlank(message = "키워드" + NOT_BLANK_MESSAGE_SUFFIX)
         String keyword,
 
         @NotBlank(message = "라이센스" + NOT_BLANK_MESSAGE_SUFFIX)
@@ -28,6 +31,7 @@ public record CreateDataSetAttributes(
         @NotBlank(message = "권한" + NOT_BLANK_MESSAGE_SUFFIX)
         String rights,
 
+        @NotBlank(message = "데이터셋 설명" + NOT_BLANK_MESSAGE_SUFFIX)
         String description
 
 ) {
