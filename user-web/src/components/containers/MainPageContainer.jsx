ed676905f8b4f46e39038255af6197168cf5a012
pangnem <sprnd645@gmail.com>
2022-07-11 11:33:30 +0900

    feat: 홈배너 추가
    See Also:
    - https://mui.com/material-ui/react-card/#media

--- user-web/src/components/containers/MainPageContainer.jsx
+++ user-web/src/components/containers/MainPageContainer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from "../presentational/header/Header";
 import MainAppBar from "../presentational/appbar/MainAppBar";
+import HomeBanner from "../presentational/banner/HomeBanner";
 
 export default function MainPageContainer() {
   return (
     <>
       <Header/>
       <MainAppBar/>
+      <HomeBanner/>
     </>
   );
 }
---
+++ user-web/src/components/presentational/banner/IntroduceCommentBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {Box, Divider} from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+export default function IntroduceCommentBox() {
+  return (
+    <Box sx={{
+      color: "white"
+    }}>
+      <Typography variant="h4">
+        Welcome to WS-AutoML.co.kr
+      </Typography>
+      <Divider sx={{
+        marginTop: "10px",
+        marginBottom: "10px",
+        border: 1,
+        borderColor: "white"
+      }}/>
+      <Typography variant="h5">
+        WS-AutoML 플랫폼은 파편화된 다양한 원천 데이터의 활용을 위한
+      </Typography>
+      <Typography variant="h5">
+        통합 데이터 관리 플랫폼으로서 공공기관과 CLUST Consortium에서
+      </Typography>
+      <Typography variant="h5">
+        구축된 데이터를 활용한 분석 및 시각화 서비스를 제공한다.
+      </Typography>
+    </Box>
+  );
+}
+
