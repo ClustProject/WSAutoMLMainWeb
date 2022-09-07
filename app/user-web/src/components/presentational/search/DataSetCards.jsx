5cf7350c9962b3ccfa1acd687a1aff22d71d2460
pangnem <sprnd645@gmail.com>
2022-09-07 17:52:34 +0900

    feat: 더미 검색페이지 메타데이터 이름 및 설명 변경

--- app/user-web/src/components/presentational/search/DataSetCards.jsx
+++ app/user-web/src/components/presentational/search/DataSetCards.jsx
@@ -5,18 +5,18 @@ import DataSetCard from './DataSetCard'
 const mockDataSetInfo = [
   {
     "id": 1,
-    "name": "a.csv",
-    "description": "해당 데이터셋에 대한 설명"
+    "name": "SURFACE_ASOS_95_MI_2022-01_2022-01_2022.csv",
+    "description": "기상강원지방기상청 춘천기상대에서 측정한 데이터로 철원지점 해발고도 155m 지점에서 측정된 자료"
   },
   {
     "id": 2,
-    "name": "b.csv",
-    "description": "해당 데이터셋에 대한 설명"
+    "name": "VDS_23_01_01_570513.csv",
+    "description": "집계일자(PK), 집계시분 Or 집계시 Or 제외(PK), 콘존ID(PK), 차로유형구분코드(PK), 평균속도\n집계일자(PK), 집계시분 Or 집계시 Or 제외(PK), 콘존ID(PK), 차로유형구분코드(PK), 평균속도"
   },
   {
     "id": 3,
-    "name": "c.csv",
-    "description": "해당 데이터셋에 대한 설명"
+    "name": "VDS_38_01_01_210028.csv",
+    "description": "집계일자(PK), 집계시분 Or 집계시 Or 제외(PK), 콘존ID(PK), 차로유형구분코드(PK), 교통량"
   }
 ];
 
