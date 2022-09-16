ef903545573120b41bdb213f58f9872b3da7d4cd
pangnem <sprnd645@gmail.com>
2022-09-16 14:17:34 +0900

    feat: 오른쪽 상단 관리자 기능 버튼 및 메뉴 관리자 전환 링크로 변경 #270

--- app/user-web/src/components/presentational/global/header/AdminUsageListItems.jsx
+++ app/user-web/src/components/presentational/global/header/AdminUsageListItems.jsx
@@ -1,38 +1,19 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import {Box, Link, Menu, Typography} from "@mui/material";
-import MenuItem from "@mui/material/MenuItem";
+import {Link, Typography} from "@mui/material";
 
 export default function AdminUsageListItems() {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Box sx={{
-      margin: "20px"
-    }}>
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={(event) => setOpen(event.currentTarget)}
-      >
-        <Typography variant="h6">관리자 기능</Typography>
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={open}
-        open={open}
-        onClose={() => setOpen(null)}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <Link
-          href="http://automl-main-web-load-balancer-213875655.ap-northeast-2.elb.amazonaws.com/metadata/management">
-          <MenuItem>메타데이터 관리</MenuItem>
-        </Link>
-      </Menu>
-    </Box>
+    <Typography
+      variant="h6"
+      sx={{
+        margin: '25px'
+      }}>
+      <Link href="http://automl-main-web-load-balancer-213875655.ap-northeast-2.elb.amazonaws.com/">
+        관리자 전환
+      </Link>
+
+    </Typography>
   );
 }
