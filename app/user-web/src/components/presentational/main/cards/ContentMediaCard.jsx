70542b50a5533f319d86d22b17f616aeefc443f9
pangnem <sprnd645@gmail.com>
2022-09-16 15:03:31 +0900

    feat: 검색 미디어 카드에 링크 추가 #270

--- app/user-web/src/components/presentational/main/cards/ContentMediaCard.jsx
+++ app/user-web/src/components/presentational/main/cards/ContentMediaCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Card from "@mui/material/Card";
-import {CardActionArea} from "@mui/material";
+import {CardActionArea, Link} from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -11,23 +11,26 @@ export default function ContentMediaCard(props) {
     <Card sx={{
       width: '29%'
     }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={props.image}
-          alt={props.name}
-          sx={{
-            height: "230px"
-          }}
-        />
-        <CardContent>
-          <Typography variant="h5" sx={{
-            textAlign: "center"
-          }}>
-            {props.name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link href={props.link}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={props.image}
+            alt={props.name}
+            sx={{
+              height: "230px"
+            }}
+          />
+          <CardContent>
+            <Typography variant="h5" sx={{
+              textAlign: "center",
+              color: 'black'
+            }}>
+              {props.name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 }
