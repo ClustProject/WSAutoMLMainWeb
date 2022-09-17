3e5e9d1d682ae87cabdd75b6c8dfe11c1030260b
pangnem <sprnd645@gmail.com>
2022-09-17 23:51:38 +0900

    feat: html 타이틀 AutoML에서 WS-AutoML로 변경 #280
    Close #280

--- app/user-web/src/pages/main/MainPage.jsx
+++ app/user-web/src/pages/main/MainPage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import MainPageContainer from "../../components/containers/MainPageContainer";
 
-const TITLE = "AutoML | 메인";
+const TITLE = "WS-AutoML | 메인";
 
 export default function MainPage() {
   useEffect(() => {
