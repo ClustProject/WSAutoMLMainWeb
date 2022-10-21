c18135603e05a53ba9b79f08f54fdf70ed5c479c
pangnem <sprnd645@gmail.com>
2022-10-21 11:08:10 +0900

    refactor: ModelLearningContent 컴포넌트 잘게 분리

--- app/user-web-prototype/src/components/presentational/model-learning/ModelLearningContent.jsx
+++ app/user-web-prototype/src/components/presentational/model-learning/ModelLearningContent.jsx
@@ -5,6 +5,7 @@ import Step from '@mui/material/Step';
 import {Divider, StepLabel, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import ModelLearningMainContent from "./ModelLearningMainContent";
+import {styled} from "@mui/system";
 
 export const CONTENT_NAME_HEIGHT = '50px';
 
@@ -20,6 +21,98 @@ const STEP_COUNT_AND_NAME_MAP = {
 const STEPS = Object.keys(STEP_COUNT_AND_NAME_MAP);
 const MIN_STEP = Math.min(...STEPS);
 const MAX_STEP = Math.max(...STEPS);
+
+const ModelLearningMainContentWrappingBox = styled(Box)({
+  backgroundColor: CONTENT_BACKGROUND_COLOR,
+  minHeight: '450px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const StepButtonsWrappingBox = styled(Box)({
+  backgroundColor: CONTENT_BACKGROUND_COLOR,
+  display: 'flex',
+  justifyContent: 'flex-end',
+  marginBottom: '110px'
+});
+
+const PreviousStepButton = (props) => {
+  const {onClick, disabled} = props;
+
+  return (
+    <Button
+      variant="outlined"
+      disabled={disabled}
+      onClick={onClick}
+    >
+      이전
+    </Button>
+  )
+}
+
+const NextStepButton = (props) => {
+  const {onClick, disabled} = props;
+
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        marginLeft: '15px'
+      }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      다음
+    </Button>
+  )
+}
+
+const StepperBox = (props) => {
+  return (
+    <Box sx={{
+      marginTop: '30px'
+    }}>
+      <Stepper
+        activeStep={props.activeStep - 1} // 0부터 시작
+        // nonLinear
+        alternativeLabel
+      >
+        {Object.values(STEP_COUNT_AND_NAME_MAP).map((it) => (
+          <Step key={it} sx={{
+            marginTop: '12px'
+          }}>
+            <StepLabel>
+              {it}
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
+
+const StepNameBox = (props) => {
+  return <Box sx={{
+    backgroundColor: CONTENT_BACKGROUND_COLOR,
+    height: CONTENT_NAME_HEIGHT
+  }}>
+    <Box sx={{
+      marginTop: '50px',
+      padding: '15px',
+    }}>
+      <Typography variant="h4">
+        {STEP_COUNT_AND_NAME_MAP[props.activeStep]}
+      </Typography>
+    </Box>
+  </Box>;
+}
+
+function ModelLearningTypography() {
+  return <Typography variant="h5">
+    모델 학습
+  </Typography>;
+}
 
 const ModelLearningContent = () => {
   const [activeStep, setActiveStep] = useState(1);
@@ -41,7 +134,7 @@ const ModelLearningContent = () => {
   // step3
   const [anyTargetVariableUsed, setAnyTargetVariableUsed] = useState(false);
 
-  function handleDisableNextButton() {
+  function handleDisableNextStepButton() {
     if (activeStep >= MAX_STEP) {
       return true;
     }
@@ -69,91 +162,35 @@ const ModelLearningContent = () => {
 
   return (
     <>
-      <Typography variant="h5">
-        모델 학습
-      </Typography>
+      <ModelLearningTypography/>
       <Divider sx={{
         marginTop: '5px'
       }}/>
-      <Box sx={{
-        marginTop: '30px'
-      }}>
-        <Stepper
-          activeStep={activeStep - 1} // 0부터 시작
-          // nonLinear
-          alternativeLabel
-        >
-          {Object.values(STEP_COUNT_AND_NAME_MAP).map((it) => (
-            <Step key={it} sx={{
-              marginTop: '12px'
-            }}>
-              <StepLabel>
-                {it}
-              </StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
-      <Box sx={{
-        backgroundColor: CONTENT_BACKGROUND_COLOR,
-        height: CONTENT_NAME_HEIGHT
-      }}>
-        <Box sx={{
-          marginTop: '50px',
-          padding: '15px',
-        }}>
-          <Typography variant="h4">
-            {STEP_COUNT_AND_NAME_MAP[activeStep]}
-          </Typography>
-        </Box>
-      </Box>
-      <Box sx={{
-        backgroundColor: CONTENT_BACKGROUND_COLOR,
-        minHeight: '450px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        {
-          <ModelLearningMainContent
-            activeStep={activeStep}
-            fileChanged={fileChanged}
-            setFileChanged={setFileChanged}
-            setAnyTargetVariableChecked={setAnyTargetVariableChecked}
-            setAnyTargetVariableUsed={setAnyTargetVariableUsed}
-          />
-        }
-      </Box>
-      <Box sx={{
-        backgroundColor: CONTENT_BACKGROUND_COLOR,
-        display: 'flex',
-        justifyContent: 'flex-end',
-        marginBottom: '110px'
-      }}>
+      <StepperBox activeStep={activeStep}/>
+      <StepNameBox activeStep={activeStep}/>
+      <ModelLearningMainContentWrappingBox>
+        <ModelLearningMainContent
+          activeStep={activeStep}
+          fileChanged={fileChanged}
+          setFileChanged={setFileChanged}
+          setAnyTargetVariableChecked={setAnyTargetVariableChecked}
+          setAnyTargetVariableUsed={setAnyTargetVariableUsed}
+        />
+      </ModelLearningMainContentWrappingBox>
+      <StepButtonsWrappingBox>
         <Box sx={{
           margin: '20px'
         }}>
-          <Button
+          <PreviousStepButton
             disabled={(activeStep <= MIN_STEP)}
-            variant="outlined"
             onClick={() => decreaseStep()}
-          >
-            이전
-          </Button>
-          <Button
-            disabled={
-              handleDisableNextButton()
-            }
-            variant="contained"
+          />
+          <NextStepButton
+            disabled={handleDisableNextStepButton()}
             onClick={() => increaseStep()}
-            sx={{
-              marginLeft: '15px'
-            }}
-          >
-            다음
-          </Button>
+          />
         </Box>
-      </Box>
+      </StepButtonsWrappingBox>
     </>
   );
 }
