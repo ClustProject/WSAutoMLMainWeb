a7179df5c520d079b26c212e0e1acc3e626f723c
pangnem <sprnd645@gmail.com>
2022-10-21 09:53:44 +0900

    fix: 하드코딩된 scale_type 컬럼 json에도 추가

--- app/user-web-prototype/src/components/presentational/model-learning/feature-selection/FeatureSelectionContent.jsx
+++ app/user-web-prototype/src/components/presentational/model-learning/feature-selection/FeatureSelectionContent.jsx
@@ -9,6 +9,7 @@ const FeatureSelectionContent = (props) => {
 
   const [content, setContent] = useState({
     "removed_missing_value_count": 0,
+    "scale_type": "standard",
     "data": []
   });
 
@@ -22,7 +23,10 @@ const FeatureSelectionContent = (props) => {
       <Typography variant="h5" align="right" sx={{
         marginBottom: '10px'
       }}>
-        데이터 정제 (제거된 결측치 : {content.removed_missing_value_count}, standard scale)
+        데이터 정제
+        ( 제거된 결측치 : {content.removed_missing_value_count},
+        {content.scale_type} scale
+        )
       </Typography>
 
       <FeatureSelectionContentTable
