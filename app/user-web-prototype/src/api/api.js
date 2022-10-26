c565ec2d67e671ef303dc292cd7dd9adf9d526d2
pangnem <sprnd645@gmail.com>
2022-10-26 16:55:13 +0900

    fix: api 요청시 LOCALHOST 접두어 제거

--- app/user-web-prototype/src/api/api.js
+++ app/user-web-prototype/src/api/api.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-const LOCALHOST_PREFIX = "http://localhost:3000";
-
 function getFeatureSelectionContent() {
   return axios
-    .get(LOCALHOST_PREFIX + "/data/feature-selection.json")
+    .get("/data/feature-selection.json")
     .then((response) => response.data);
 }
 
 function getNavigationContent() {
   return axios
-    .get(LOCALHOST_PREFIX + "/data/data-navigation-content.json")
+    .get("/data/data-navigation-content.json")
     .then((response) => response.data);
 }
 
 function getModelLearningResult() {
   return axios
-    .get(LOCALHOST_PREFIX + "/data/model-learning-result.json")
+    .get("/data/model-learning-result.json")
     .then((response) => response.data);
 }
 
