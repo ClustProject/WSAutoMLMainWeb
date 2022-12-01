78587aaf9fde1e97feff9891da360a98f9c4718e
pangnem <sprnd645@gmail.com>
2022-12-01 15:17:43 +0900

    feat: 상세페이지 데이터 다운로드 링크 추가 #337
    Close #337

--- app/user-web-prototype/src/components/presentational/detail/MetaDataDownloadButton.jsx
+++ app/user-web-prototype/src/components/presentational/detail/MetaDataDownloadButton.jsx
@@ -2,9 +2,14 @@ import Button from "@mui/material/Button";
 import React from "react";
 
 function MetaDataDownloadButton() {
-  return <Button variant="outlined">
-    CSV 파일 다운로드
-  </Button>;
+  return (
+    <Button
+      variant="outlined"
+      href={"https://automl-file-storage.s3.ap-northeast-2.amazonaws.com/20220804-041252-SURFACE_ASOS_95_MI_2022-01_2022-01_2022.csv"}
+    >
+      CSV 파일 다운로드
+    </Button>
+  );
 }
 
 export default MetaDataDownloadButton;
